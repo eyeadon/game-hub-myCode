83129b88e9590da4c45c1e5de42f43549f9fe379
eyeadon <eyeadon40@gmail.com>
2023-12-15 12:12:31 -0500

    fetch games

---
+++ src/services/api-client.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// axios instance
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "d3da0a5c41f84d899720ab939875da2e",
+  },
+});
