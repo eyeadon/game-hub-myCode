93c0f7d5a1a9aa2d69e8a0b4dce249c843146a0d
eyeadon <eyeadon40@gmail.com>
2023-12-18 16:52:10 -0500

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
 // importing return value of api-client.ts, making variable name for it
+// axios instance
 import apiClient from "../services/api-client";
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  //                  weird design from API side
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -18,9 +27,11 @@ const useGames = () => {
   const [error, setError] = useState("");
 
   useEffect(() => {
+    // DOM API: AbortController interface represents a controller object that allows you to abort one or more Web requests as and when desired.
     const controller = new AbortController();
 
     apiClient
+      //                       endpoint
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => setGames(res.data.results))
       .catch((err) => {
