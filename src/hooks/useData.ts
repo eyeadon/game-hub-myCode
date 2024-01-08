fa1c080a0f151c84eb2d7fc5ad22720a13f6f5c6
eyeadon <eyeadon40@gmail.com>
2024-01-08 15:23:26 -0500

    filter games by genre button

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,63 +2,55 @@ import { useEffect, useState } from "react";
 // • importing return value of api-client.ts, making variable name for it "apiClient"
 // • axios instance
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  // params
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // DOM API: AbortController interface represents a controller object that allows you to abort one or more Web requests as and when desired.
-    const controller = new AbortController();
+  useEffect(
+    () => {
+      // DOM API: AbortController interface represents a controller object that allows you to abort one or more Web requests as and when desired.
+      const controller = new AbortController();
 
-    setLoading(true);
+      setLoading(true);
 
-    apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-      .then((res) => {
-        setData(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
+      apiClient
+        .get<FetchResponse<T>>(endpoint, {
+          // AxiosRequestConfig<any>
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
+        });
 
-    // // error handling from axios docs
-    // .catch(function (err) {
-    //   if (err.response) {
-    //     // The request was made and the server responded with a status code
-    //     // that falls out of the range of 2xx
-    //     console.log(err.response.data);
-    //     console.log(err.response.status);
-    //     console.log(err.response.headers);
-    //     // our code
-    //     setError(err.response);
-    //     setLoading(false);
-    //     // removed axios request code
-    //   } else {
-    //     // Something happened in setting up the request that triggered an Error
-    //     console.log("Error", `"${err.message}"`);
-    //     // our code
-    //     setError(err.message);
-    //     setLoading(false);
-    //   }
-    //   console.log(err.config);
-    // });
+      // cleanup function
+      return () => controller.abort();
 
-    // cleanup function
-    return () => controller.abort();
-
-    // included an array of dependencies, without this requests are contstantly sent to backend
-  }, []);
+      // included an array of dependencies, without this requests are contstantly sent to backend
+      // [] -> fetch data only once, when the component is rendered
+      // [deps] -> if any of these dependencies change, effect will rerun and fetch data from the server
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
