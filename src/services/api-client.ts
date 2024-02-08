064516e9b8d223b8da32e8216cbe900b513a82b9
eyeadon <eyeadon40@gmail.com>
2024-02-08 18:13:58 -0500

    adding react query functionality

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,34 @@
 import axios from "axios";
 
-// axios instance created
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "d3da0a5c41f84d899720ab939875da2e",
   },
 });
+
+// class APIClient<T> {
+//   endpoint: string;
+
+//   constructor(endpoint: string) {
+//     this.endpoint = endpoint;
+//   }
+
+//   // this issue with callbacks, could do the following in constructor
+//   // if it's a regular function:
+//   // this.getAll = this.getAll.bind(this);
+
+//   //  returns response object
+//   // arrow functions do not have their own this context
+//   getAll = () => {
+//     return axiosInstance.get<T[]>(this.endpoint).then((res) => res.data);
+//   };
+
+//   post = (data: T) => {
+//     return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data);
+//   };
+// }
+
+// export default APIClient;
+
+export default axiosInstance;
